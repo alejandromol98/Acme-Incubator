6b2a76aebc013e212813131f0cc733aa4c2f1341
alejandromol98 <alejandromol98@gmail.com>
2020-08-18 20:19:18 +0200

    @Size validation added to all string attributes from all the entities.

--- src/main/java/acme/entities/molinaBulletins/MolinaBulletin.java
+++ src/main/java/acme/entities/molinaBulletins/MolinaBulletin.java
@@ -8,6 +8,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 
 import acme.framework.entities.DomainEntity;
 import lombok.Getter;
@@ -24,12 +25,15 @@ public class MolinaBulletin extends DomainEntity {
 	// Attributes ------------------------------------------------------------
 
 	@NotBlank
+	@Size(min = 0, max = 255)
 	private String				title;
 
 	@NotBlank
+	@Size(min = 0, max = 255)
 	private String				author;
 
 	@NotBlank
+	@Size(min = 0, max = 255)
 	private String				text;
 
 	@Temporal(TemporalType.TIMESTAMP)
